96ab97b36347b5336c3c16926e203821c1cbc188
Ronan C <rojocas@gmail.com>
2020-04-07 12:45:12 +0000

    Provided choices for product categories

---
+++ products/choices.py
@@ -0,0 +1,83 @@
+
+BRAND_CHOICES = (
+    ('LABONE', 'Label One'),
+    ('LABTWO', 'Label Two'),
+    ('LABTHR', 'Label Three'),
+    ('LABFOU', 'Label Four'),
+    ('LABFIV', 'Label Five'),
+    ('LABSIX', 'Label Six'),
+    ('LABSEV', 'Label Seven'),
+)
+
+PERSON_CATEGORY_CHOICES = (
+    ('M', 'Men'),
+    ('W', 'Women'),
+    ('K', 'Kids'),
+)
+
+PRODUCT_CATEGORY_CHOICES = (
+    ('Men', (
+        ('MJACPAR', 'Parka'),
+        ('MJACCOA', 'Coat'),
+        ('MJACJAC', 'Jacket'),
+        ('MSWESWE', 'Sweatshirt'),
+        ('MSWEHOO', 'Hoodie'),
+        ('MSHILON', 'Long Sleeve Shirt'),
+        ('MSHISHO', 'Short Sleeve Shirt'),
+        ('MTSHLON', 'Long Sleeve T-Shirt'),
+        ('MTSHSHO', 'Short Sleeve T-Shirt'),
+        ('MTSHPOL', 'Polo Shirt'),
+        ('MPANJEA', 'Jeans'),
+        ('MPANCAR', 'Cargo'),
+        ('MPANCHI', 'Chino'),
+        ('MPANSHO', 'Shorts'),
+        ('MACCBEA', 'Beanie'),
+        ('MACCCAP', 'Cap'),
+        ('MACCBAG', 'Bag'),
+        ('MACCBEL', 'Belt'),
+        ('MACCWAL', 'Wallet'),
+        ('MACCSUN', 'Sunglasses'),
+        )
+     ),
+    ('Women', (
+        ('WJACCOA', 'Coat'),
+        ('WJACJAC', 'Jacket'),
+        ('WSWESWE', 'Sweatshirt'),
+        ('WSWEHOO', 'Hoodie'),
+        ('WSHILON', 'Long Sleeve Shirt'),
+        ('WSHISHO', 'Short Sleeve Shirt'),
+        ('WTSHLON', 'Long SLeeve T-Shirt'),
+        ('WTSHSHO', 'Short Sleeve T-Shirt'),
+        ('WDREDRE', 'Dress'),
+        ('WDRESKI', 'Skirt'),
+        ('WPANJEA', 'Jeans'),
+        ('WPANCAR', 'Cargo'),
+        ('WPANSHO', 'Shorts'),
+        ('WACCBAG', 'Bag'),
+        ('WACCSWI', 'Swimsuit'),
+        ('WACCJEW', 'Jewelry'),
+        )
+     ),
+    ('Kids', (
+        ('KJACJAC', 'Jacket'),
+        ('KSEWSWE', 'Sweatshirt'),
+        ('KSWEHOO', 'Hoodie'),
+        ('KSHILON', 'Long Sleeve Shirt'),
+        ('KSHISHO', 'Short Sleeve Shirt'),
+        ('KTSHLON', 'Long SLeeve T-Shirt'),
+        ('KTSHSHO', 'Short Sleeve T-Shirt'),
+        ('KPANJEA', 'Jeans'),
+        ('KPANSHO', 'Shorts'),
+        )
+     ),
+)
+
+SIZE_CHOICES = (
+    ('NA', 'Not Applicable'),
+    ('XS', 'Extra Small'),
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+    ('XL', 'Extra Large'),
+    ('XXL', 'Extra Extra Large'),
+)
