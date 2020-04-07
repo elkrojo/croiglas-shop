32c20bd6120dfca9310f35f591b82942dcebba03
Ronan C <rojocas@gmail.com>
2020-04-07 12:46:36 +0000

    Migrations updated

--- products/models.py
+++ products/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+import products.choices as choices
+
+BRAND_CHOICES = choices.BRAND_CHOICES
+PRODUCT_CATEGORY_CHOICES = choices.PRODUCT_CATEGORY_CHOICES
+SIZE_CHOICES = choices.SIZE_CHOICES
 
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=100, default='')
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    brand = models.CharField(max_length=6,
+                             choices=BRAND_CHOICES,
+                             default='LABONE')
+    category = models.CharField(max_length=7,
+                                choices=PRODUCT_CATEGORY_CHOICES,
+                                default='MJACPAR')
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=False)
+    size = models.CharField(max_length=3, choices=SIZE_CHOICES, default='NA')
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
     image = models.ImageField(upload_to='images')
 
     def __str__(self):
-        return self.name
+        return self.title
