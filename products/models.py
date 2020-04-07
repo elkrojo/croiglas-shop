60de0570a094f77a4f9cb6591c03e5d6ceb7eba5
Ronan C <rojocas@gmail.com>
2020-04-07 17:48:48 +0000

    Added more fields to product model

--- products/models.py
+++ products/models.py
@@ -11,13 +11,23 @@ class Product(models.Model):
     brand = models.CharField(max_length=6,
                              choices=BRAND_CHOICES,
                              default='LABONE')
+
     category = models.CharField(max_length=7,
                                 choices=PRODUCT_CATEGORY_CHOICES,
                                 default='MJACPAR')
+
     title = models.CharField(max_length=100, blank=False)
+
     description = models.TextField(blank=False)
-    size = models.CharField(max_length=3, choices=SIZE_CHOICES, default='NA')
-    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
+
+    size = models.CharField(max_length=3,
+                            choices=SIZE_CHOICES,
+                            default='NA')
+
+    price = models.DecimalField(max_digits=6,
+                                decimal_places=2,
+                                blank=False)
+
     image = models.ImageField(upload_to='images')
 
     def __str__(self):
