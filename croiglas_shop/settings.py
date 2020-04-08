f8ad58db12b4aca2de9e88e4066e97272cfe0c01
Ronan C <rojocas@gmail.com>
2020-04-08 17:27:53 +0000

    Added conditional django secret key depending on build running

--- croiglas_shop/settings.py
+++ croiglas_shop/settings.py
@@ -24,7 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+if "DEV_SECRET_KEY" in os.environ:
+    SECRET_KEY = os.environ.get('DEV_SECRET_KEY')
+else:
+    SECRET_KEY = os.environ.get('PRO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if "DATABASE_URL" in os.environ:
