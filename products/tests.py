20038ecddf209d635923718518dc59d7c173775b
Ronan C <rojocas@gmail.com>
2020-04-08 17:09:32 +0000

    Changed test field to match  current field names

--- products/tests.py
+++ products/tests.py
@@ -10,5 +10,5 @@ class ProductTests(TestCase):
     """
 
     def test_str(self):
-        test_name = Product(name='A product')
-        self.assertEqual(str(test_name), 'A product')
+        test_title = Product(title='A product')
+        self.assertEqual(str(test_title), 'A product')
