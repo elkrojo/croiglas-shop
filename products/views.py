cce8233fe5b7a4a6a34b0fc0c2386ba3aee6cbd5
Ronan C <rojocas@gmail.com>
2020-04-17 12:55:49 +0000

    Adedd product showcase view

--- products/views.py
+++ products/views.py
@@ -6,3 +6,8 @@ from .models import Product
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+
+
+def product_showcase(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, "product-showcase.html", {"product": product})
