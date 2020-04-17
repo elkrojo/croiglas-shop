d8829c89afc1536de3185871a1440bae0fd851f3
Ronan C <rojocas@gmail.com>
2020-04-17 12:52:16 +0000

    Added url for product showcase

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
-from .views import all_products
+from .views import all_products, product_showcase
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^product_showcase/(?P<id>\d+)',
+        product_showcase,
+        name='product_showcase'),
 ]
