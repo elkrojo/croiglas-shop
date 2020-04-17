f0f3baa650d14a2b463e830be9ded4f77390dec0
Ronan C <rojocas@gmail.com>
2020-04-17 16:54:42 +0000

    Uncommented static location to connect external static directory

--- croiglas_shop/settings.py
+++ croiglas_shop/settings.py
@@ -164,8 +164,8 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL = None
 
 # Must be active for production
-# STATICFILES_LOCATION = 'static'
-# STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
