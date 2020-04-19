f5f71c172579c51e543db50ae8015164da645e0d
Ronan C <rojocas@gmail.com>
2020-04-19 18:45:19 +0000

    Added specific url for all products

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from .views import all_products, product_showcase
 
 urlpatterns = [
-    url(r'^$', all_products, name='products'),
+    url(r'^all_products$', all_products, name='products'),
     url(r'^product_showcase/(?P<id>\d+)',
         product_showcase,
         name='product_showcase'),
