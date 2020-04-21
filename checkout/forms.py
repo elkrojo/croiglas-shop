974147dd12d3e7f82e69185115e814a3de1e0325
Ronan C <rojocas@gmail.com>
2020-04-21 16:47:41 +0000

    Rearranged & renamed form fields

--- checkout/forms.py
+++ checkout/forms.py
@@ -25,6 +25,12 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = (
-            'full_name', 'phone_number', 'country', 'postcode',
-            'town_or_city', 'street_address1', 'street_address2',
-            'county')
+            'full_name',
+            'street_address_1',
+            'street_address_2',
+            'city',
+            'postcode',
+            'county',
+            'country',
+            'phone_number',
+            )
