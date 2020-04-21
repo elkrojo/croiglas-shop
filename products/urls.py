ea4348b4d5d0230f8491f1bd7853a20fbc891b5e
Ronan C <rojocas@gmail.com>
2020-04-21 12:51:42 +0000

    Created urls for gender categories

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url, include
-from .views import all_products, product_showcase
+from .views import (all_products,
+                    mens_products,
+                    womens_products,
+                    product_showcase
+                    )
 
 urlpatterns = [
-    url(r'^all_products$', all_products, name='products'),
+    url(r'^$', all_products, name='products'),
+    url(r'^men', mens_products, name='mens_products'),
+    url(r'^women', womens_products, name='womens_products'),
     url(r'^product_showcase/(?P<id>\d+)',
         product_showcase,
         name='product_showcase'),
