5dae945b3f08c53290cc68163c108e14cd96e2e9
Ronan C <rojocas@gmail.com>
2020-04-21 12:52:43 +0000

    Created views for gender categories

--- products/views.py
+++ products/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, GenderFilter
 
 
 # Create your views here.
 def all_products(request):
     products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
+
+
+def mens_products(request):
+    category = GenderFilter.objects.filter(category="M")
+    products = Product.objects.filter(person_category=category)
+    return render(request, "products.html", {"products": products})
+
+
+def womens_products(request):
+    category = GenderFilter.objects.filter(category="W")
+    products = Product.objects.filter(person_category=category)
     return render(request, "products.html", {"products": products})
 
 
