f1ab6ef16e43dce00f8140e16b87d4e4dd52fa36
Ronan C <rojocas@gmail.com>
2020-04-26 13:16:02 +0000

    Expanded profile view to incorporate a mutable customer model

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib import messages, auth
 from django.core.urlresolvers import reverse
-from .forms import UserLoginForm, UserRegistrationForm
 from django.template.context_processors import csrf
 from django.contrib.auth.decorators import login_required
+from .forms import UserLoginForm, UserRegistrationForm, CustomerForm
+from .models import Customer
 
 
 # Create your views here.
@@ -12,15 +13,21 @@ from django.contrib.auth.decorators import login_required
 #     return render(request, "index.html")
 
 
+@login_required
 def logout(request):
-    """A view that logs the user out and redirects back to the index page"""
+    """
+    A view that logs the user out and redirects
+    back to the index page
+    """
     auth.logout(request)
     messages.success(request, 'You have successfully logged out')
     return redirect(reverse('index'))
 
 
 def login(request):
-    """A view that manages the login form"""
+    """
+    A view that manages the login form
+    """
     if request.method == 'POST':
         user_form = UserLoginForm(request.POST)
         if user_form.is_valid():
@@ -29,7 +36,7 @@ def login(request):
 
             if user:
                 auth.login(request, user)
-                messages.error(request, "You have successfully logged in")
+                messages.success(request, "You have successfully logged in")
 
                 if request.GET and request.GET['next'] != '':
                     next = request.GET['next']
@@ -37,7 +44,8 @@ def login(request):
                 else:
                     return redirect(reverse('index'))
             else:
-                user_form.add_error(None, "Your username or password are incorrect")
+                user_form.add_error(None,
+                                    "Your username or password are incorrect")
     else:
         user_form = UserLoginForm()
 
@@ -45,18 +53,22 @@ def login(request):
     return render(request, 'login.html', args)
 
 
-@login_required
-def profile(request):
-    """A view that displays the profile page of a logged in user"""
-    return render(request, 'profile.html')
+# @login_required
+# def profile(request):
+#     """
+#     A view that displays the profile page of a logged in user
+#     """
+#     return render(request, 'profile.html')
 
 
 def register(request):
-    """A view that manages the registration form"""
+    """
+    A view that manages the registration form
+    """
     if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            user_form.save()
+        register_form = UserRegistrationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
 
             user = auth.authenticate(request.POST.get('email'),
                                      password=request.POST.get('password1'))
@@ -69,7 +81,31 @@ def register(request):
             else:
                 messages.error(request, "unable to log you in at this time!")
     else:
-        user_form = UserRegistrationForm()
+        register_form = UserRegistrationForm()
 
-    args = {'user_form': user_form}
+    args = {'register_form': register_form}
     return render(request, 'register.html', args)
+
+
+@login_required
+def profile(request):
+    """
+    Shows the customer their current profile details.
+    Allows the customer to update their profile details.
+    Creates a new customer on completion.
+    """
+
+    customer = Customer.objects.filter(user=request.user).first()
+    if request.method == "POST":
+        profile_form = CustomerForm(request.POST, instance=customer)
+        if profile_form.is_valid():
+            customer = profile_form.save(commit=False)
+            customer.user = request.user
+            customer.save()
+            messages.success(request,
+                             "Your profile has been updated successfully")
+    else:
+        profile_form = CustomerForm(instance=customer)
+
+    args = {"profile_form": profile_form}
+    return render(request, "profile.html", args)
