8dfe1adc2b0f000b1d216355d8622260c7a3a367
Ronan C <rojocas@gmail.com>
2020-04-26 13:18:24 +0000

    Created customemr model in accounts models

---
+++ accounts/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User)
+    full_name = models.CharField(max_length=42)
+    street_address_1 = models.CharField(max_length=32)
+    street_address_2 = models.CharField(max_length=32)
+    city = models.CharField(max_length=24)
+    postcode = models.CharField(max_length=12)
+    county = models.CharField(max_length=24)
+    country = models.CharField(max_length=32)
+    phone_number = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.user.email
