75219c56a1ae82c1cf82aaebf84c3da7643a2c6d
Ronan C <rojocas@gmail.com>
2020-04-29 16:20:07 +0000

    Added test for product showcase

--- products/tests.py
+++ products/tests.py
@@ -9,6 +9,17 @@ class ProductTests(TestCase):
     Product model
     """
 
-    def test_str(self):
+    def test_product_title(self):
         test_title = Product(title='A product')
         self.assertEqual(str(test_title), 'A product')
+
+    def test_product_showcase(self):
+        test_product = Product.objects.create(
+            title="A product",
+            price=99.99,
+            size="L",
+            description="A description.",
+        )
+
+        response = self.client.get(f"/products/product_showcase/{test_product.id}")
+        self.assertEqual(response.status_code, 200)
