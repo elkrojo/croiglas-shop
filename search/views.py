d9e60b9818e121cd8c4b82637bfacb7fc4881c62
Ronan C <rojocas@gmail.com>
2020-04-30 14:32:00 +0000

    Redirect to home page if search is empty

--- search/views.py
+++ search/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.contrib import messages
 from products.models import Product
 
 
 # Create your views here.
 def do_search(request):
     products = Product.objects.filter(title__icontains=request.GET['q'])
-    return render(request, "products.html", {"products": products})
+    if products:
+        return render(request, "products.html", {"products": products})
+    else:
+        return render(request, "index.html")
